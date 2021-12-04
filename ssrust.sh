de578c3c21e5dffd0cb90b82b7bb8a3bdc02364c
Zhehao Wang <wangzhehao410305@gmail.com>
2021-12-04 19:28:14 +0800

    ssrust: add argparse and print server url

--- ssrust.sh
+++ ssrust.sh
@@ -3,7 +3,7 @@
 # Deploy latest ss-server with silent_drop_replay customization to avoid easy detection.
 # tcp_only, chacha20-ietf-poly1305, random pwd
 # Ubuntu 20.04
-# wget -O - https://raw.githubusercontent.com/zhehaowang/FaaS/master/ssrust.sh | bash
+# wget -O - https://raw.githubusercontent.com/zhehaowang/FaaS/master/ssrust.sh | bash -s "-p 443"
 
 green='\033[0;32m'
 red='\033[0;31m'
@@ -11,11 +11,24 @@ yellow='\033[1;33m'
 nc='\033[0m' # No Color'
 
 port=443
-if [ $# -eq 0 ]; then
-    echo -e "${yellow}No port specified, using default 443${nc}"
-else
-    port=$1
-fi
+source="aws"
+while getopts ":s:p:" opt; do
+  case $opt in
+    s) source="$OPTARG"
+    ;;
+    p) port="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    exit 1
+    ;;
+  esac
+
+  case $OPTARG in
+    -*) echo "Option $opt needs a valid argument"
+    exit 1
+    ;;
+  esac
+done
 
 sudo apt update
 sudo apt install docker.io --yes
@@ -26,12 +39,22 @@ pwd=$(openssl rand -base64 16)
 enc="chacha20-ietf-poly1305"
 dockername="ssserver-rust-$port"
 
+serveraddr="0.0.0.0"
+if [ $source -eq "aws" ]; then
+    serveraddr=$(curl http://checkip.amazonaws.com)
+    echo -e "${yellow}On AWS, using ${serveraddr} to generate ss url ${nc}"
+else
+    echo -e "${yellow}Not on AWS, using ${serveraddr} to generate ss url ${nc}"
+fi
+
+
 # as of 20211204, silently dropping replay packets seemed to have made shadowsocks easily detectable:
 # https://github.com/shadowsocks/shadowsocks-rust/pull/556
 # https://github.com/net4people/bbs/issues/69
 # https://github.com/net4people/bbs/issues/22
 # for this reason we set silent_drop_replay false by default
 # \"silent_drop_replay\": false,
+mkdir -p ~/ss/
 echo "
 {
     \"server\":\"0.0.0.0\",
@@ -41,9 +64,9 @@ echo "
     \"password\":\"$pwd\",
     \"timeout\":120,
     \"method\":\"$enc\"
-}" > ~/ss.conf
+}" > ~/ss/$dockername.conf
 
-sudo docker run --name $dockername --restart always -p $port:$port/tcp -v ~/ss.conf:/etc/shadowsocks-rust/config.json -dit ghcr.io/shadowsocks/ssserver-rust:latest
+sudo docker run --name $dockername --restart always -p $port:$port/tcp -v ~/ss/$dockername.conf:/etc/shadowsocks-rust/config.json -dit ghcr.io/shadowsocks/ssserver-rust:latest
 ret=$?
 
 # sudo docker container ls
@@ -55,15 +78,21 @@ if [ $ret -eq 0 ]; then
     echo -e "${yellow}Docker log:${nc}"
     sudo docker logs $containerid
 
+    if [ ! -f ~/geturl.py ]; then
+        wget -O - https://raw.githubusercontent.com/Drickle/ss-uri-generator/master/ssuri-gen.py > ~/geturl.py
+    fi
+
+    ssurl=$(python3 ~/geturl.py <(sed "s/\"server\":\"0.0.0.0\"/\"server\":\"$serveraddr\"/g" ~/ss/$dockername.conf) | tail -1)
+    
     echo -e "
     ${green}Done${nc}!
     password:           ${yellow}${pwd}${nc},
     port:               ${yellow}${port}${nc},
     docker instance id: ${yellow}${containerid}${nc},
-    encryption:         ${yellow}${enc}${nc}"
+    encryption:         ${yellow}${enc}${nc},
+    ss-url:             ${yellow}${ssurl}${nc}"
 
-    mkdir -p ~/ss/
-    cat "${pwd}" > ~/ss/$dockername
+    echo ssurl > ~/ss/$dockername.url
 else
     echo -e "${red}Docker instance failed to start, ret ${ret} ${nc}"
 fi
