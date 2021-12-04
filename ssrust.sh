ac9eb896a0735b7ca6afa73ec879ba131cc8a44f
Zhehao Wang <wangzhehao410305@gmail.com>
2021-12-04 08:58:27 +0800

    ssrust: add status report

--- ssrust.sh
+++ ssrust.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # Deploy latest ss-server with silent_drop_replay customization to avoid easy detection.
+# tcp_only, chacha20-ietf-poly1305, random pwd
 # Ubuntu 20.04
 
 green='\033[0;32m'
@@ -10,7 +11,7 @@ nc='\033[0m' # No Color'
 
 port=443
 if [ $# -eq 0 ]; then
-    echo "No port specified, using default 443"
+    echo -e "${yellow}No port specified, using default 443${nc}"
 else
     port=$1
 fi
@@ -42,18 +43,26 @@ echo "
 }" > ~/ss.conf
 
 sudo docker run --name $dockername --restart always -p $port:$port/tcp -v ~/ss.conf:/etc/shadowsocks-rust/config.json -dit ghcr.io/shadowsocks/ssserver-rust:latest
+ret=$?
+
 # sudo docker container ls
-containerid=$(sudo docker ps -f name=$dockername --format {{.ID}})
 
-echo -e "${yellow}Docker log:${nc}"
-sudo docker logs $containerid
+if [ $ret -eq 0 ]; then
+    containerid=$(sudo docker ps -f name=$dockername --format {{.ID}})
+    echo -e "${yellow}Docker instance ${containerid} started.${nc}"
 
-echo -e "
-${green}Done${nc}!
-password:           ${yellow}${pwd}${nc},
-port:               ${yellow}${port}${nc},
-docker instance id: ${yellow}${containerid}${nc},
-encryption:         ${yellow}${enc}${nc}"
+    echo -e "${yellow}Docker log:${nc}"
+    sudo docker logs $containerid
+
+    echo -e "
+    ${green}Done${nc}!
+    password:           ${yellow}${pwd}${nc},
+    port:               ${yellow}${port}${nc},
+    docker instance id: ${yellow}${containerid}${nc},
+    encryption:         ${yellow}${enc}${nc}"
+else
+    echo -e "${red}Docker instance failed to start, ret ${ret} ${nc}"
+fi
 
 # on mac to test a port
 # nc -vnz [ip] $port
